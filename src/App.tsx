c9a5e8d36ae8dd2f03c7c6f36e781e1fc009a0ab
Mukesh Kumar <144682316+MukeshKumar0101@users.noreply.github.com>
2024-10-06 13:30:54 +0530

    started working on project netflix gpt

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl text-blue-700 text-center font-bold">Started nextGPT project</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
